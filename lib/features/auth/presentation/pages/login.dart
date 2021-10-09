34b07a9dc0841ef11826f3e02cb029dac0418fa6
Jonathanvanhaaften <Jonathanvanhaaften@gmail.com>
2021-10-09 11:51:20 -0600

    baisc login page

--- lib/features/auth/presentation/pages/login.dart
+++ lib/features/auth/presentation/pages/login.dart
@@ -12,18 +12,35 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: const Text('Login'),
+          title: const Text('Count to Quit'),
         ),
-        body: ListView(
-          children: <Widget>[
-            Text(
-              "Login",
-              style: Theme.of(context).textTheme.headline5,
-            ),
-            const TextField(
-              decoration: InputDecoration(hintText: "email"),
-            )
-          ],
+        body: Center(
+          child: ListView(
+            shrinkWrap: true,
+            padding: const EdgeInsets.all(16.0),
+            children: <Widget>[
+              Text(
+                "Login",
+                style: Theme.of(context).textTheme.headline5,
+              ),
+              const SizedBox(height: 20.0),
+              const TextField(
+                decoration: InputDecoration(hintText: "email"),
+              ),
+              const SizedBox(height: 10.0),
+              const TextField(
+                obscureText: true,
+                decoration: InputDecoration(hintText: "password"),
+              ),
+              const SizedBox(
+                height: 10.0,
+              ),
+              ElevatedButton(
+                child: const Text("login"),
+                onPressed: () {},
+              )
+            ],
+          ),
         ));
   }
 }
