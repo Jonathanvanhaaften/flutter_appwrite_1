2e401d23fdd0074c6160186359e32385f0e2b509
Jonathanvanhaaften <Jonathanvanhaaften@gmail.com>
2021-10-17 16:06:35 -0600

    basic stage of app next is saving to database

--- lib/features/general/presentation/pages/home.dart
+++ lib/features/general/presentation/pages/home.dart
@@ -1,18 +1,80 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+// class HomePage extends StatelessWidget {
+//   const HomePage({Key? key}) : super(key: key);
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('Count to Quit'),
+//       ),
+//       body: SingleChildScrollView(
+//         child: Column(
+//           crossAxisAlignment: CrossAxisAlignment.start,
+//           children: const <Widget>[],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  void _decrementCounter() {
+    setState(() {
+      _counter--;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Count to Quit'),
       ),
-      body: SingleChildScrollView(
+      body: Center(
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: const <Widget>[],
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Smoke count for Today:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
         ),
       ),
+      floatingActionButton:
+          Column(mainAxisAlignment: MainAxisAlignment.end, children: [
+        FloatingActionButton(
+          onPressed: _incrementCounter,
+          tooltip: 'Add a smoke',
+          child: const Icon(Icons.add),
+          heroTag: null,
+        ),
+        FloatingActionButton(
+          onPressed: _decrementCounter,
+          tooltip: 'Increment',
+          child: const Icon(Icons.remove),
+          heroTag: null,
+        ),
+      ]),
     );
   }
 }
