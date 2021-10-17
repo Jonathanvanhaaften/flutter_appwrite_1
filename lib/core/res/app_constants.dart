921a87a0dc27d48ddbf59c6a04a03d4e5898393b
Jonathanvanhaaften <Jonathanvanhaaften@gmail.com>
2021-10-17 12:49:44 -0600

    added string conncetion for data base

--- lib/core/res/app_constants.dart
+++ lib/core/res/app_constants.dart
@@ -1,4 +1,5 @@
 class AppConstants {
   static const String projectId = "615e11d72252b";
   static const String endpoint = "http://10.0.2.2:4003/v1";
+  static const String collectionId = "615f32ace8ada";
 }
